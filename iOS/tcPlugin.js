4f9248d5fbf45ff411b150dcabdbcfd149d3440c
Stevie Graham <sjtgraham@mac.com>
2012-03-21 18:20:02 -0700

    refactor js

--- iOS/tcPlugin.js
+++ iOS/tcPlugin.js
@@ -1,103 +1,110 @@
-var TwilioPlugin = {
-    Device: function() {
-        this.delegate = {}
-        return this;
-    },
-    
-    Connection: function() {
-        this.delegate = {}
-        return this;
+(function() {
+    var delegate = {}
+    var TwilioPlugin = {
+        Device: function() {
+            return this;
+        },
+
+        Connection: function() {
+            return this;
+        }
     }
-}
 
-TwilioPlugin.Device.prototype.setup = function(token) {
-    // Take a token and instantiate a new device object
-    var self = this;
-    
-    var error = function(callback) {
-    
+    TwilioPlugin.Device.prototype.setup = function(token) {
+        // Take a token and instantiate a new device object
+        var error = function(callback) {
+
+        }
+
+        var success = function(callback) {
+            var argument = callback['arguments'] || new Twilio.Connection();
+            if (delegate[callback['callback']]) delegate[callback['callback']](argument);
+        }
+
+        Cordova.exec(success, error, "TCPlugin", "deviceSetup", [token]);
     }
-    
-    var success = function(callback) {
-        var argument = callback['arguments'] || new Twilio.Connection();
-        if(self.delegate[callback['callback']]) self.delegate[callback['callback']](argument);
+
+    // polymorphic function. if called with function as an argument, the function is invoked
+    // when a connection has been established. if called with an object a connection is established with those options
+    TwilioPlugin.Device.prototype.connect = function(argument) {
+        if (typeof(argument) == 'function') {
+            delegate['onconnect'] = argument;
+        } else if (typeof(argument) == 'object') {
+            Cordova.exec("TCPlugin.connect", argument)
+        }
     }
-    
-    Cordova.exec(success, error, "TCPlugin", "deviceSetup", [token]);
-}
 
-// polymorphic function. if called with function as an argument, the function is invoked
-// when a connection has been established. if called with an object a connection is established with those options
-TwilioPlugin.Device.prototype.connect = function(argument) {
-    if(typeof(argument) == 'function') {
-        this.delegate['onconnect'] = argument;
-    } else if(typeof(argument) == 'object') {
-        Cordova.exec("TCPlugin.connect", argument)
+    TwilioPlugin.Device.prototype.disconnectAll = function() {
+        alert('disconnect');
+        Cordova.exec('TCPlugin.disconnectAll');
     }
-}
 
-TwilioPlugin.Device.prototype.disconnectAll = function() {
-	Cordova.exec('TCPlugin.disconnectAll');
-}
+    TwilioPlugin.Device.prototype.disconnect = function(fn) {
+        delegate['ondisconnect'] = fn;
+    }
 
-TwilioPlugin.Device.prototype.disconnect = function(fn) {
-    this.delegate['ondisconnect'] = fn;
-}
+    TwilioPlugin.Device.prototype.ready = function(fn) {
+        delegate['onready'] = fn;
+    }
 
-TwilioPlugin.Device.prototype.ready = function(fn) {
-    this.delegate['onready'] = fn;
-}
+    TwilioPlugin.Device.prototype.offline = function(fn) {
+        delegate['onoffline'] = fn;
+    }
 
-TwilioPlugin.Device.prototype.offline = function(fn) {
-    this.delegate['onoffline'] = fn;
-}
+    TwilioPlugin.Device.prototype.incoming = function(fn) {
+        delegate['onincoming'] = fn;
+    }
 
-TwilioPlugin.Device.prototype.incoming = function(fn) {
-    this.delegate['onincoming'] = fn;
-}
+    TwilioPlugin.Device.prototype.cancel = function(fn) {
+        delegate['oncancel'] = fn;
+    }
 
-TwilioPlugin.Device.prototype.cancel = function(fn) {
-    this.delegate['oncancel'] = fn;
-}
+    TwilioPlugin.Device.prototype.error = function(fn) {
+        delegate['onerror'] = fn;
+    }
 
-TwilioPlugin.Device.prototype.error = function(fn) {
-    this.delegate['onerror'] = fn;
-}
+    TwilioPlugin.Device.prototype.presence = function(fn) {
+        delegate['onpresence'] = fn;
+    }
 
-TwilioPlugin.Device.prototype.presence = function(fn) {
-    this.delegate['onpresence'] = fn;
-}
+    TwilioPlugin.Device.prototype.status = function() {
 
-TwilioPlugin.Device.prototype.status = function() {
-	
-}
+        }
 
-TwilioPlugin.Device.prototype.sounds = {
-    incoming:   function(boolean) {},
-    outgoing:   function(boolean) {},
-    disconnect: function(boolean) {}
-}
+    TwilioPlugin.Device.prototype.sounds = {
+        incoming: function(boolean) {},
+        outgoing: function(boolean) {},
+        disconnect: function(boolean) {}
+    }
 
-TwilioPlugin.Connection.prototype.accept = function(fn) {}
+    TwilioPlugin.Connection.prototype.accept = function(argument) {
+        if (typeof(argument) == 'function') {
+            delegate['onaccept'] = argument;
+        } else {
+            Cordova.exec("TCPlugin.acceptConnection")
+        }
+    }
 
-TwilioPlugin.Connection.prototype.reject = function(fn) {}
+    TwilioPlugin.Connection.prototype.reject = function(fn) {}
 
-TwilioPlugin.Connection.prototype.disconnect = function(fn) {}
+    TwilioPlugin.Connection.prototype.disconnect = function(fn) {}
 
-TwilioPlugin.Connection.prototype.error = function(fn) {}
+    TwilioPlugin.Connection.prototype.error = function(fn) {}
 
-TwilioPlugin.Connection.prototype.mute = function() {}
+    TwilioPlugin.Connection.prototype.mute = function() {}
 
-TwilioPlugin.Connection.prototype.unmute = function() {}
+    TwilioPlugin.Connection.prototype.unmute = function() {}
 
-TwilioPlugin.Connection.prototype.sendDigits = function(string) {}
+    TwilioPlugin.Connection.prototype.sendDigits = function(string) {}
 
-TwilioPlugin.Connection.prototype.status = function() {}
+    TwilioPlugin.Connection.prototype.status = function() {}
 
-TwilioPlugin.install = function() {
-    if (!window.Twilio) window.Twilio = {}; 
-    if (!window.Twilio.Device) window.Twilio.Device = new TwilioPlugin.Device();
-    if (!window.Twilio.Connection) window.Twilio.Connection = TwilioPlugin.Connection;
-}
+    TwilioPlugin.install = function() {
+        if (!window.Twilio) window.Twilio = {};
+        if (!window.Twilio.Device) window.Twilio.Device = new TwilioPlugin.Device();
+        if (!window.Twilio.Connection) window.Twilio.Connection = TwilioPlugin.Connection;
+    }
 
-Cordova.addConstructor(TwilioPlugin.install);
+    Cordova.addConstructor(TwilioPlugin.install);
+
+})()
